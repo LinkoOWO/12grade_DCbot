7972dfd746d60f66f4fd37b5b0afc53a317cfbf4
LinkoOWO <160370815+LinkoOWO@users.noreply.github.com>
2026-09-22 12:02:29 +0800

    Add files via upload

---
+++ cogs/engagement.py
@@ -0,0 +1,12 @@
+import discord
+from discord.ext import commands
+from discord import app_commands
+from discord import Embed
+import asyncio
+
+class Engagement(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+
+async def setup(bot):
+    await bot.add_cog(Engagement(bot))
